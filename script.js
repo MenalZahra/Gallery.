072a1a437a10f4a40da4daaf2f893d6004f84243
MenalZahra <anfalcheema15@gmail.com>
2024-09-12 00:19:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('JavaScript Console');
+var fullImgBox=document.getElementById("fullImgBox");
+var fullImg=document.getElementById("fullImg");
+
+function openFullImg(pic){
+    fullImgBox.style.display="flex";
+    fullImg.src=pic;
+}
+
+function closeFullImg(){
+    fullImgBox.style.display="none";
+}
